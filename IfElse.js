b4689080c07e728cb66d28d751f4764c60df5654
Eeshu Narayan <eeshunarayan15@gmail.com>
2024-12-21 00:16:23 +0530

    updating js files

---
+++ IfElse.js
@@ -0,0 +1,11 @@
+
+// single line conditional statement
+// if (13 > 14) console.log("true");
+
+
+
+// if (12 > 13) {
+//     console.log("greater than")
+// } else {
+//     console.log("less than")
+// }
---
+++ ifelseif.js
@@ -0,0 +1,15 @@
+// if (13 > 12) {
+// console.log("13>12")
+    
+// } else if (14 > 12) {
+//     console.log("14 > 12")
+
+// }else if (17 > 15) {
+//     console.log("17>15")
+// }
+// else {
+//     console.log("else block")
+    
+// }
+
+// yaha jo condition true ho gai to aage check he nahi hoga
