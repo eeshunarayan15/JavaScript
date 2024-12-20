fa7a7428df857dd31fce15af72ac714faddf168f
Eeshu Narayan <eeshunarayan15@gmail.com>
2024-12-20 23:38:43 +0530

    updating js file

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 //types matlab data ka roop
+//harsh Stirng
+//12 integer
+//12.3;
+// 22.0
+// true Boolean
+
+
+//types primitive data type || reference data type
+
+
+// []{}() refrence data type
+
+// var a = 12
+// var a = 12.1 primitive
+// var a = true
+// var a = []
+
+// var a = 12
+// var b=a
+// var arr = [1, 2, 3, 4, 5, 6]
+// var b = arr
+// b.pop()
+// console.log(arr)
+// console.log(b)
+//yaha ye problem ke value a aur b dono se data delete ho ja raha hai
+//refrence data type ka copy nahi hota yaha ham uska refrece copy hota hai
+//
+//ham isko copy karne ke liye ham Spread operator ka use karnenge
+// var arr = [1, 2, 3, 4, 5, 6]
+// var b = [...arr]
+// b.pop()
+// console.log(b)
+// console.log(arr)
+// output
+// [1, 2, 3, 4, 5]
+//  [1, 2, 3, 4, 5, 6]
+
+// yaha arr ke value mai koi change nahi aaya hai
+
+//conditionals
+
+// if
+//     else
+//     else if
+// ternary operator
+// switch
+
+
+//loops
+//
